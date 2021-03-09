6d1e296e9b843f61a21e830519698d9291d0d852
CrabBot <zacharykeepers@gmail.com>
2021-03-09 20:45:11 +0000

    efSseadf

--- Planet-Adventure/game.py
+++ Planet-Adventure/game.py
@@ -3,13 +3,32 @@ import world
 from player import Player
 from pathlib import Path
 import pickle
+import sys
+import time
+import random
 
 
 def play(saved_world=None, saved_player=None):
     if saved_world and saved_player:
+      start = input('Would you like to start or go to the market. (Type m = market or s = start)\n')
+      if start == 's':
+        typing ('Starting game...\n\n\n\n')
+        time.sleep(3)
         world._world = saved_world
         player = saved_player
+      elif start == 'm':
+        typing(' market ')
+        
     else:
+      start = input('Would you like to start or go to the market. (Type m = market or s = start)\n')
+      if start == 's':
+        typing ('Starting game...\n\n\n\n')
+        time.sleep(3)
+        world._world = saved_world
+        player = saved_player
+      elif start == 'm':
+        typing(' market ')
+        
         world.load_tiles()
         player = Player()
     game_loop(player)
@@ -21,7 +40,7 @@ def game_loop(player):
         room = world.tile_exists(player.location_x, player.location_y)
         room.modify_player(player)
         if player.is_alive() and not player.victory:
-            print("Choose an action:\n")
+            ("Choose an action:\n")
             available_actions = room.available_actions()
             for action in available_actions:
                 print(action)
@@ -54,5 +73,16 @@ def check_for_save():
     else:
         play()
 
-if __name__ == "__main__":
-    check_for_save()
+typingspeed = 100
+
+def typing(text):
+	for letter in text:
+		sys.stdout.write(letter)
+		sys.stdout.flush()
+		time.sleep(random.random()*6/typingspeed)
+
+typing('Welecome to Planet Adventure!!!\n\n')
+check_for_save()
+
+
+
--- Planet-Adventure/tiles.py
+++ Planet-Adventure/tiles.py
@@ -36,11 +36,11 @@ class MapTile:
 
 class StartingRoom(MapTile):
     def intro_text(self):
-        return """
+        return ("""
         You find yourself on a planet with a thick cloud of fog!
         You can make out four paths.
         Which way should you go.
-        """
+        """)
 
     def modify_player(self, the_player):
         pass
@@ -48,9 +48,9 @@ class StartingRoom(MapTile):
 
 class EmptyPath(MapTile):
     def intro_text(self):
-        return """
+        return ("""
         Another featureless part of the planet. You must venture onwards.
-        """
+        """)
 
     def modify_player(self, the_player):
         pass
@@ -73,30 +73,30 @@ class FindBlasterRoom(LootRoom):
         super().__init__(x, y, items.Blaster())
 
     def intro_text(self):
-        return """
+        return ("""
         You notice something shiny in the corner of a cliff.
         It's a blaster! You pick it up.
-        """
+        """)
 
 class FindSwordRoom(LootRoom):
     def __init__(self, x, y):
         super().__init__(x, y, items.Celestial_Sword())
 
     def intro_text(self):
-        return """
+        return ("""
         You see a bright light coming from a rock,
         It's the Celestial Sword, that has the power of the cosmos!
-        """
+        """)
 
 class FindBazookaRoom(LootRoom):
     def __init__(self, x, y):
         super().__init__(x, y, items.Laser_Bazooka())
 
     def intro_text(self):
-        return """
+        return ("""
         You notice something shiny and giant laying next to a rock.
         It's a Laser Bazooka! You pick it up.
-        """
+        """)
 
 
 class Find5GoldRoom(LootRoom):
@@ -104,9 +104,9 @@ class Find5GoldRoom(LootRoom):
         super().__init__(x, y, items.Gold(5))
 
     def intro_text(self):
-        return """
+        return ("""
         Someone dropped 5 gold pieces. You pick them up.
-        """
+        """)
 
 
 class EnemyRoom(MapTile):
@@ -117,7 +117,7 @@ class EnemyRoom(MapTile):
     def modify_player(self, the_player):
         if self.enemy.is_alive():
             the_player.hp = the_player.hp - self.enemy.damage
-            print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
+            return("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
 
     def available_actions(self):
         if self.enemy.is_alive():
@@ -132,13 +132,13 @@ class MartianRoom(EnemyRoom):
 
     def intro_text(self):
         if self.enemy.is_alive():
-            return """
+            return ("""
             A martian jumps down from a rock in front of you!
-            """
+            """)
         else:
-            return """
+            return ("""
             The body of a dead martian lays on the ground.
-            """
+            """)
 
 
 class DragonRoom(EnemyRoom):
@@ -147,13 +147,13 @@ class DragonRoom(EnemyRoom):
 
     def intro_text(self):
         if self.enemy.is_alive():
-            return """
+            return ("""
             A dragon is blocking your path!
-            """
+            """)
         else:
-            return """
+            return ("""
             A dead dragon reminds you of your triumph.
-            """
+            """)
 
 class KnightRoom(EnemyRoom):
     def __init__(self, x, y):
@@ -161,13 +161,13 @@ class KnightRoom(EnemyRoom):
 
     def intro_text(self):
         if self.enemy.is_alive():
-            return """
+            return ("""
             The Celestial Knight is blocking your path!
-            """
+            """)
         else:
-            return """
+            return ("""
             The dead Knight reminds you of your success!
-            """
+            """)
 
 class HippieRoom(EnemyRoom):
     def __init__(self, x, y):
@@ -175,22 +175,22 @@ class HippieRoom(EnemyRoom):
 
     def intro_text(self):
         if self.enemy.is_alive():
-            return """
+            return ("""
             The King of the planet, the Evil Psycho Hippie, is blocking your path!
-            """
+            """)
         else:
-            return """
+            return ("""
             A dead King reminds you of your victory!
-            """
+            """)
 
 
 class SnakePitRoom(MapTile):
     def intro_text(self):
-        return """
+        return ("""
         You have fallen into a pit of deadly snakes!
 
         You have died!
-        """
+        """)
 
     def modify_player(self, player):
         player.hp = 0
@@ -198,13 +198,13 @@ class SnakePitRoom(MapTile):
 
 class PortalRoom(MapTile):
     def intro_text(self):
-        return """
+        return ("""
         You see a bright light in the distance...
         ... it grows as you get closer! It's a portal to Earth!!!
 
 
         Victory is yours!
-        """
+        """)
 
     def modify_player(self, player):
         player.victory = True
